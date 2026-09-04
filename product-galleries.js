362e57939a447517398c2ed643f8ef7b7b6a3f34
zhb5621949 <zhb5621949@gmail.com>
2026-09-04 08:58:16 +0800

    启用桌面原图预览 V19

--- product-galleries.js
+++ product-galleries.js
@@ -1,210 +1,46 @@
 window.PRODUCT_GALLERIES = {
-  "hard-ring-cookbook": [
-    "images/gallery/source-13.jpg",
-    "images/gallery/source-14.jpg",
-    "images/gallery/source-15.jpg",
-    "images/gallery/source-16.jpg",
-    "images/gallery/source-17.jpg",
-    "images/gallery/source-19.jpg",
-    "images/gallery/source-12.jpg"
-  ],
-  "deluxe-inner-nail-cookbook": [
-    "images/gallery/source-18.jpg",
-    "images/gallery/source-24.jpg",
-    "images/gallery/source-25.jpg",
-    "images/gallery/source-27.jpg",
-    "images/gallery/source-12.jpg"
-  ],
-  "deluxe-outer-nail-cookbook": [
-    "images/gallery/source-18.jpg",
-    "images/gallery/source-24.jpg",
-    "images/gallery/source-25.jpg",
-    "images/gallery/source-27.jpg",
-    "images/gallery/source-12.jpg"
-  ],
-  "butterfly-cookbook": [
-    "images/gallery/source-20.jpg",
-    "images/gallery/source-21.jpg",
-    "images/gallery/source-22.jpg",
-    "images/gallery/source-23.jpg",
-    "images/gallery/source-26.jpg",
-    "images/gallery/source-12.jpg"
-  ],
-  "spiral-cookbook": [
-    "images/gallery/source-28.jpg",
-    "images/gallery/source-29.jpg",
-    "images/gallery/source-30.jpg",
-    "images/gallery/source-31.jpg",
-    "images/gallery/source-32.jpg",
-    "images/gallery/source-33.jpg",
-    "images/gallery/source-34.jpg"
-  ],
-  "laminated-spiral-cookbook": [
-    "images/gallery/source-28.jpg",
-    "images/gallery/source-29.jpg",
-    "images/gallery/source-30.jpg",
-    "images/gallery/source-31.jpg",
-    "images/gallery/source-32.jpg",
-    "images/gallery/source-33.jpg",
-    "images/gallery/source-34.jpg"
-  ],
-  "saddle-album-cookbook": [
-    "images/gallery/source-18.jpg",
-    "images/gallery/source-24.jpg",
-    "images/gallery/source-25.jpg",
-    "images/gallery/source-27.jpg",
-    "images/gallery/source-12.jpg"
-  ],
-  "perfect-album-cookbook": [
-    "images/gallery/source-18.jpg",
-    "images/gallery/source-24.jpg",
-    "images/gallery/source-25.jpg",
-    "images/gallery/source-27.jpg",
-    "images/gallery/source-12.jpg"
-  ],
-  "additional-inner-pages": [
-    "images/gallery/source-59.jpg",
-    "images/gallery/source-60.jpg",
-    "images/gallery/source-61.jpg",
-    "images/gallery/source-62.jpg",
-    "images/gallery/source-63.jpg"
-  ],
+  "hard-ring-cookbook": ["images/product-originals/hard-ring-cookbook.jpg"],
+  "deluxe-inner-nail-cookbook": ["images/product-originals/deluxe-inner-nail-cookbook.jpg"],
+  "deluxe-outer-nail-cookbook": ["images/product-originals/deluxe-outer-nail-cookbook.png"],
+  "butterfly-cookbook": ["images/product-originals/butterfly-cookbook.jpg"],
+  "spiral-cookbook": ["images/product-originals/spiral-cookbook.jpg"],
+  "laminated-spiral-cookbook": ["images/product-originals/laminated-spiral-cookbook.jpg"],
+  "saddle-album-cookbook": ["images/product-originals/saddle-album-cookbook.jpg"],
+  "perfect-album-cookbook": ["images/product-originals/perfect-album-cookbook.png"],
+  "additional-inner-pages": ["images/product-originals/additional-inner-pages.jpg"],
+
   "laminated-menu": [
-    "images/gallery/source-03.jpg",
-    "images/gallery/source-05.jpg",
-    "images/gallery/source-04.jpg"
+    "images/product-originals/laminated-menu.jpg",
+    "images/product-originals/laminated-menu-a3.jpg"
   ],
-  "pvc-menu": [
-    "images/gallery/source-08.jpg",
-    "images/gallery/source-09.jpg",
-    "images/gallery/source-06.jpg",
-    "images/gallery/source-07.jpg"
-  ],
-  "photo-menu": [
-    "images/gallery/source-02.jpg"
-  ],
-  "photo-paper-menu": [
-    "images/gallery/source-02.jpg"
-  ],
-  "checklist-menu": [
-    "images/gallery/source-43.jpg",
-    "images/gallery/source-44.jpg",
-    "images/gallery/source-45.jpg",
-    "images/gallery/source-46.jpg",
-    "images/gallery/source-47.jpg"
-  ],
-  "multipart-form": [
-    "images/gallery/source-43.jpg",
-    "images/gallery/source-44.jpg",
-    "images/gallery/source-45.jpg",
-    "images/gallery/source-46.jpg",
-    "images/gallery/source-47.jpg"
-  ],
-  "indoor-light-film-menu": [
-    "images/gallery/source-39.jpg",
-    "images/gallery/source-40.jpg",
-    "images/gallery/source-41.jpg",
-    "images/gallery/source-42.jpg"
-  ],
-  "takeaway-card": [
-    "images/gallery/source-35.jpg",
-    "images/gallery/source-36.jpg",
-    "images/gallery/source-37.jpg",
-    "images/gallery/source-38.jpg"
-  ],
-  "voucher": [
-    "images/gallery/source-35.jpg",
-    "images/gallery/source-36.jpg",
-    "images/gallery/source-37.jpg",
-    "images/gallery/source-38.jpg"
-  ],
-  "table-sign-menu": [
-    "images/gallery/source-53.jpg",
-    "images/gallery/source-55.jpg",
-    "images/gallery/source-56.jpg",
-    "images/gallery/source-57.jpg",
-    "images/gallery/source-58.jpg"
-  ],
-  "banner-menu": [
-    "images/gallery/source-10.jpg",
-    "images/gallery/source-11.jpg"
-  ],
-  "kt-board-menu": [
-    "images/gallery/source-50.jpg",
-    "images/gallery/source-51.jpg",
-    "images/gallery/source-52.jpg",
-    "images/gallery/source-54.jpg"
-  ],
+  "pvc-menu": ["images/product-originals/pvc-menu.jpg"],
+  "photo-menu": ["images/product-originals/photo-menu.jpg"],
+  "photo-paper-menu": ["images/product-originals/photo-menu.jpg"],
+  "checklist-menu": ["images/product-originals/checklist-menu.jpg"],
+  "multipart-form": ["images/product-originals/category-menu.png"],
+  "indoor-light-film-menu": ["images/product-originals/indoor-light-film-menu.jpg"],
+  "takeaway-card": ["images/product-originals/takeaway-card.jpg"],
+  "voucher": ["images/product-originals/voucher.jpg"],
+  "table-sign-menu": ["images/product-originals/table-sign-menu.jpg"],
+  "banner-menu": ["images/product-originals/banner-menu.jpg"],
+  "kt-board-menu": ["images/product-originals/kt-board-menu.jpg"],
+
   "backlit-fabric": [
-    "images/gallery/source-39.jpg",
-    "images/gallery/source-40.jpg",
-    "images/gallery/source-41.jpg",
-    "images/gallery/source-42.jpg"
+    "images/product-originals/backlit-fabric.jpg",
+    "images/product-originals/backlit-fabric-2.jpg"
   ],
-  "stretch-lightbox-fabric": [
-    "images/gallery/source-39.jpg",
-    "images/gallery/source-40.jpg",
-    "images/gallery/source-41.jpg",
-    "images/gallery/source-42.jpg"
-  ],
-  "canvas": [
-    "images/gallery/source-48.jpg",
-    "images/gallery/source-49.jpg"
-  ],
-  "business-card": [
-    "images/gallery/source-35.jpg",
-    "images/gallery/source-36.jpg",
-    "images/gallery/source-37.jpg",
-    "images/gallery/source-38.jpg"
-  ],
-  "flyer": [
-    "images/gallery/source-43.jpg",
-    "images/gallery/source-44.jpg",
-    "images/gallery/source-45.jpg",
-    "images/gallery/source-46.jpg",
-    "images/gallery/source-47.jpg",
-    "images/gallery/source-35.jpg",
-    "images/gallery/source-36.jpg"
-  ],
-  "kt-board-poster": [
-    "images/gallery/source-50.jpg",
-    "images/gallery/source-51.jpg",
-    "images/gallery/source-52.jpg",
-    "images/gallery/source-54.jpg"
-  ],
-  "sticker-poster": [
-    "images/gallery/source-02.jpg"
-  ],
-  "photo-poster": [
-    "images/gallery/source-02.jpg"
-  ],
-  "banner": [
-    "images/gallery/source-10.jpg",
-    "images/gallery/source-11.jpg"
-  ],
-  "indoor-light-film": [
-    "images/gallery/source-39.jpg",
-    "images/gallery/source-40.jpg",
-    "images/gallery/source-41.jpg",
-    "images/gallery/source-42.jpg"
-  ],
-  "static-cling": [
-    "images/gallery/source-02.jpg"
-  ],
-  "one-way-vision": [
-    "images/gallery/source-02.jpg"
-  ],
-  "car-sticker": [
-    "images/gallery/source-02.jpg"
-  ],
-  "glass-lettering": [
-    "images/gallery/source-02.jpg"
-  ],
-  "copy-paper-print": [
-    "images/gallery/source-59.jpg",
-    "images/gallery/source-60.jpg",
-    "images/gallery/source-61.jpg",
-    "images/gallery/source-62.jpg",
-    "images/gallery/source-63.jpg"
-  ]
+  "stretch-lightbox-fabric": ["images/product-originals/stretch-lightbox-fabric.jpg"],
+  "canvas": ["images/product-originals/canvas.jpg"],
+  "business-card": ["images/product-originals/business-card.jpg"],
+  "flyer": ["images/product-originals/flyer.jpg"],
+  "kt-board-poster": ["images/product-originals/kt-board-poster.jpg"],
+  "sticker-poster": ["images/product-originals/sticker-poster.jpg"],
+  "photo-poster": ["images/product-originals/photo-poster.jpg"],
+  "banner": ["images/product-originals/banner.jpg"],
+  "indoor-light-film": ["images/product-originals/indoor-light-film.jpg"],
+  "static-cling": ["images/product-originals/static-cling.jpg"],
+  "one-way-vision": ["images/product-originals/one-way-vision.jpg"],
+  "car-sticker": ["images/product-originals/car-sticker.jpg"],
+  "glass-lettering": ["images/product-originals/glass-lettering.png"],
+  "copy-paper-print": ["images/product-originals/copy-paper-print.jpg"],
 };
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "photo-pricing-shell-v18";
+const CACHE_NAME = "photo-pricing-shell-v19";
 const APP_SHELL = [
   "./",
   "./index.html",
